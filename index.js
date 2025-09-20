9c34534bb7fa9e8b793a0bd1f42abba18e596c95
Arun Vangapally <arunvangapally81@gmail.com>
2025-09-20 21:54:16 +0530

    Added HTML/CSS practice files

---
+++ index.js
@@ -0,0 +1,31 @@
+//function scope
+var num1=20, num2=3, name="arun";
+
+function multiply(){
+  return num1*num2;
+}
+
+var a=9;
+ var fun = function(){
+  console.log("printing", a);
+  var a=10;
+ }
+ fun();
+
+ function square(num1, num2){
+  console.log("S")
+ }
+// console.log(multiply()); //60
+
+//block scope
+function getScore(){
+  var num1=2, num2=3;
+  function add(){
+    return name+ "scored" + (num1 + num2);
+  }
+  return add();
+}
+// console.log(getScore()); //arun scored 5
+
+
+
